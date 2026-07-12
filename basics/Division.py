1c151da7ac3d6b47d6da2e468d61b7d5a8f598d9
sanskrutiiii <sanskrutiawatade04@gmail.com>
2026-07-12 16:06:18 +0530

    Division.py

---
+++ basics/Division.py
@@ -0,0 +1,6 @@
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = round(x / y, 2)
+#here I have to round off the output by 2 digits soo I have added ,2 in the round function
+#round(number[, ndgits]) is the documentation given...
+print(z)
