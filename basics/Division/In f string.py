e38aa677c2cffc415c0a61b48a056175d84b2d5a
sanskrutiiii <sanskrutiawatade04@gmail.com>
2026-07-12 16:21:58 +0530

    Delete basics/Division directory

--- basics/Division/In f string.py
+++
@@ -1,8 +0,0 @@
-#If this were going to be a big integer I might have used comma like before to separate each triple of numbers with commas
-
-x = float(input("What's x? "))
-y = float(input("What's y? "))
-z = x / y
-prin(f"{z:.2f}")
-
-#I have used a diffeence sequence of character This the the way according to the documentation how you specify using a f string how many digits you want to print
