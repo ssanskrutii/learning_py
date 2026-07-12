90dc086038a8c5dccaed9a20defd2a41269a7eb6
sanskrutiiii <sanskrutiawatade04@gmail.com>
2026-07-12 15:32:29 +0530

    int&float data type.py

---
+++ basics/int&float data type.py
@@ -0,0 +1,22 @@
+#int is an integer like -1,0,1 , etc
+"""
+x = input("What's x? ")
+y = input("What's y? ")
+
+z = int(x) + int(y)
+print(z)
+"""
+
+
+"""
+#float is a number with a decimal point, properly called a floating point
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+print(x + y)
+"""
+
+
+#To make the code look smaller I'll write it as
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+print(x + y)
