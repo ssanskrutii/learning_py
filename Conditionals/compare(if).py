ce58d20798c9d69833ffe18a6db0afc1943f031e
sanskruti Awatade <sanskrutiawatade04@gmail.com>
2026-07-18 21:51:24 +0530

    compare(if).py

---
+++ Conditionals/compare(if).py
@@ -0,0 +1,12 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y") 
+#here i have pressed the space bar for 4 times or just presed 'tab' the reason is to tell python only exicute this if the statement above is true
+
+if x > y:
+    print("x is greater than y")
+
+if x == y:
+    print("x is equal to y")
